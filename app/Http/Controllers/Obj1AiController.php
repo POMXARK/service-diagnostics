e290538642cadb98b04bb48a4482c4fd54d75065
Бушуев Роман <1997pom@gmail.com>
2023-04-30 10:06:37 +1000

    Initial commit

---
+++ app/Http/Controllers/Obj1AiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreObj1AiRequest;
+use App\Http\Requests\UpdateObj1AiRequest;
+use App\Models\Obj1Ai;
+
+class Obj1AiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreObj1AiRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreObj1AiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Obj1Ai  $obj1Ai
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Obj1Ai $obj1Ai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Obj1Ai  $obj1Ai
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Obj1Ai $obj1Ai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateObj1AiRequest  $request
+     * @param  \App\Models\Obj1Ai  $obj1Ai
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateObj1AiRequest $request, Obj1Ai $obj1Ai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Obj1Ai  $obj1Ai
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Obj1Ai $obj1Ai)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Obj1CmnController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreObj1CmnRequest;
+use App\Http\Requests\UpdateObj1CmnRequest;
+use App\Models\Obj1Cmn;
+
+class Obj1CmnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreObj1CmnRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreObj1CmnRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Obj1Cmn  $obj1Cmn
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Obj1Cmn $obj1Cmn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Obj1Cmn  $obj1Cmn
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Obj1Cmn $obj1Cmn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateObj1CmnRequest  $request
+     * @param  \App\Models\Obj1Cmn  $obj1Cmn
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateObj1CmnRequest $request, Obj1Cmn $obj1Cmn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Obj1Cmn  $obj1Cmn
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Obj1Cmn $obj1Cmn)
+    {
+        //
+    }
+}
